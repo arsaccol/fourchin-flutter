219c7bcf586000c30af1dca87b782d6ac9821b21
Alexandre R. Saccol <arsaccol@gmail.com>
2019-12-06 16:54:44 -0300

    Copy boards widet code into its own class

---
+++ lib/boards.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class BoardsScreen extends StatelessWidget {
+  final boardsUrl = 'https://a.4cdn.org/boards.json';
+  dynamic boardsData = null;
+  
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: http.get(boardsUrl),
+      builder: (context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.done) {
+          this.boardsData = jsonDecode(snapshot.data.body)['boards'];
+          return ListView.builder(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(10.0),
+            itemCount: boardsData.length,
+            itemBuilder: (context, i) {
+              return Card(
+                child: ListTile(
+                  title: Text(
+                    this.boardsData[i]['title'].toString(),
+                  ),
+                ),
+              );
+            },
+
+          );
+        } //if snapshot etc.
+        
+        else {
+          return Text(
+            'Getting...'
+
+          );
+
+        }
+
+      }
+
+    );
+
+
+
+
+
+  }
+
+}
